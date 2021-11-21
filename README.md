a063e09113a0f3b5b460f1552e25a454b9d90092
Sergey Petrunin <halfstrik@gmail.com>
2021-11-21 02:40:54 -0500

    Added sync files script

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 Here is my Gentoo GNU/Linux config for MacBook Air 2012,
 created mostly for educational purposes and to help keep track of my setup.
 
+## Sync files
+```shell
+su -
+cd /home/sergey/Projects/gentoo-config/
+python syncfiles.py
+```
+
 ## TODOs
  * Wireless card uses open-source driver which may cause problems.
  * Screen brightness goes to max after lid re-open.
---
+++ syncfiles.py
@@ -0,0 +1,69 @@
+import os
+import subprocess
+
+HASH_CMD = 'sha256sum'
+HASH_LEN = 64
+
+# Colors in terminal: https://stackoverflow.com/a/39452138
+CBOLD = '\33[1m'
+CGREEN = '\33[32m'
+CRED = '\033[91m'
+CEND = '\033[0m'
+
+
+def traverse_and_print(root):
+    print(f'Executing for {root}')
+    for (dir_path, dir_names, file_names) in os.walk(root):
+        for file_name in file_names:
+            src_file_path = f'{dir_path}/{file_name}'
+            src_file_hash = get_file_hash(src_file_path)
+            dst_file_path = f'/{dir_path}/{file_name}'
+            dst_file_hash = get_file_hash(dst_file_path)
+            print(f'Found file {CBOLD}{dir_path}/{file_name}{CEND}')
+
+            if src_file_hash == dst_file_hash:
+                print('  no changes detected, skipping...')
+            else:
+                while True:
+                    action = ask_confirmation()
+                    if action == 'diff':
+                        print_difference(src_file_path, dst_file_path)
+                    elif action == 'yes':
+                        overwrite_file(src_file_path, dst_file_path)
+                        break
+                    else:
+                        print('  Choose to skip...')
+                        break
+
+
+def get_file_hash(full_path):
+    result = subprocess.run([HASH_CMD, full_path], capture_output=True, text=True)
+    return result.stdout[:HASH_LEN]
+
+
+def ask_confirmation() -> str:
+    while True:
+        answer = input(f'  files are different, overwrite? [{CGREEN}yes{CEND}/{CRED}no{CEND}/diff] ')
+        if answer not in ('yes', 'no', 'diff'):
+            continue
+        return answer
+
+
+def print_difference(src_file_path, dst_file_path):
+    subprocess.run(['diff', '--unified', '--color', src_file_path, dst_file_path])
+    print(f'    {CBOLD}--- end diff ---{CEND}')
+
+
+def overwrite_file(src_file_path, dst_file_path):
+    result = subprocess.run(['rsync', '--verbose', src_file_path, dst_file_path], capture_output=True, text=True)
+    if result.returncode == 0:
+        print(f'  File {CBOLD}{dst_file_path}{CEND} was {CGREEN}successfully{CEND} overwritten')
+    else:
+        print(result.stderr)
+        print(f'  File {CBOLD}{dst_file_path}{CEND} was {CRED}not{CEND} overwritten')
+        input('  Press any key to continue...')
+
+
+if __name__ == '__main__':
+    print('Execution')
+    traverse_and_print('etc')
