5a057c9b512c490b9ad527b1e066c3073a980d63
Sergey Petrunin <halfstrik@gmail.com>
2021-11-29 22:18:01 -0500

    Added simple scripts to enable/disable bluetooth

---
+++ home/sergey/sound_with_speakers.sh
@@ -0,0 +1,5 @@
+echo 'Run as root:'
+echo 'rc-service bluetooth stop'
+echo 'rc-service bluealsa stop'
+
+rm /home/sergey/.asoundrc
---
+++ home/sergey/sound_with_tozo.sh
@@ -0,0 +1,5 @@
+echo 'Run as root:'
+echo 'rc-service bluetooth start'
+echo 'rc-service bluealsa start'
+
+cp /home/sergey/.asoundrc_tozo_t10 /home/sergey/.asoundrc
